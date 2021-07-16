5bc41aacd3bfa94b2db1f3a8ad1daee68c154d62
vkrh0406 <vkrh0406@gmail.com>
2021-07-16 16:20:15 +0900

    스프링 타입 컨버터시작

--- loginstart/src/main/java/hello/login/web/HomeController.java
+++ loginstart/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Slf4j
@@ -45,7 +48,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+   // @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         Member member = (Member)sessionManager.getSession(request);
@@ -57,6 +60,61 @@ public class HomeController {
 
 
         model.addAttribute("member", member);
+
+        return "loginHome";
+
+    }
+
+    //@GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+
+        if (loginMember == null) {
+            return "home";
+        }
+        //로그인
+
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
+
+    }
+
+    //@GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMBER,required = false) Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+        //로그인
+
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+        //로그인
+
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
 
         return "loginHome";
 
--- loginstart/src/main/java/hello/login/web/login/LoginController.java
+++ loginstart/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,11 +11,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -51,7 +56,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "lohin/loginForm";
@@ -75,9 +80,73 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+    //@PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "lohin/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+
+        if (loginMember == null) {
+            bindingResult.reject("LoginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        //로그인 성공
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
+
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request,
+                          @RequestParam(defaultValue = "/") String redirectURL) {
+        if (bindingResult.hasErrors()) {
+            return "lohin/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+
+        if (loginMember == null) {
+            bindingResult.reject("LoginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        //로그인 성공
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
+        return "redirect:" + redirectURL;
+    }
+
+   // @PostMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         sessionManager.expire(request);
+
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
 
         return "redirect:/";
     }
